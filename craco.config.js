f247a5d408439a2548ead142ebaf05301eb71a89
see91 <wangqizhen@fancyfuture.com.cn>
2020-08-07 15:50:11 +0800

    ⚙️ alias

--- craco.config.js
+++ craco.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   webpack: {
     alias: {
-      '@': path.resolve('src')
+      "@": path.resolve(__dirname, 'src')
     },
     plugins: [],
     configure: (webpackConfig, { env, paths }) => { return webpackConfig; }
